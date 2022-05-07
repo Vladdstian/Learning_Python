5d69bbc2cc61ae3c35d8287ce5908a8af7d43bcf
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-05-07 20:09:00 +0300

    learn to print ;)

--- Day 1/Basics/main.py
+++ Day 1/Basics/main.py
@@ -1,3 +1,12 @@
-print("Hello, World!")
+print("Hello, World! - classic! right?")
+print('this is another type of print where I\'m using this \'')
+print('the backslash \\ is used as an escape character')
+print("""to summarize: 
+* ' + " = OK 
+* " + " or ' + ' = NOK 
+* \\n to create a new line
+* "*3 to create a multiple line print
+* \\ and any character that cannot be used inside a string = OK -> escape character """)
+print(" and that's all folks!")
 
 # this comment is to try if things work as they should
