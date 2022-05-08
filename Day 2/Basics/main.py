e7564342005823e54b7e8a41aaab6053293c0b5e
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-05-08 11:06:26 +0300

    ...going further with the information today

--- Day 2/Basics/main.py
+++ Day 2/Basics/main.py
@@ -12,5 +12,21 @@ print("""to name the variables, in python there are some rules:
 * the name of a variable can have multiple names but they should be separated in Python with '_'
 * you can have numbers in the names of variables, but they shouldn't start with a number""")
 
+# primitive data types learned: strings, integer (int), floating point number (float), boolean (bool)
+string_example = "this is a string"
+string2_example = '123'  # this is also a string
+int_example = 123  # this is an int
+float_example = 3.14159  # this is a float example and a bit of PI's value
+float2_example = 734_529.687  # did you know a big number can be separated by underscore "_" ?
+bool_example = True  # this is a bool example - it can have 2 values only: either True or False
+print(float2_example)
+
+# Type error, Type checking and Type conversion
+type(int_example)  # this function will result in class 'int' because it is of type integer
+str(float2_example)  # this function will convert the variable from a float to a string
+# to change a data type into another data type we use this function data_type_we_want(data_type_we_have)
 
 
+# string manipulation
+len(string_example)  # gives the length of a string - using another data type like int will give an error
+string_example[0]  # string subscripting - will return the letter at the index 0 inside the []
