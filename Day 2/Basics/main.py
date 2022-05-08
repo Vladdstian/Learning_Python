e781378ddbf87d2bf2e11aaa5f56f2c98a729604
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-05-08 09:49:33 +0300

    added pycharm setup files that i keep deleting :(

---
+++ Day 2/Basics/main.py
@@ -0,0 +1,9 @@
+# take input from the user
+
+name = input("What is your name?\n").lower().capitalize()
+input(f"Well, {name}, do you know how to take input from a user?")
+
+print(f"I will tell you my dear {name}.")
+print("You just have to use the function input() built into Python")
+print("It's that easy!")
+print(f'And, {name}, keep it a secret but you can assign it to a variable and use it whnever you want in your code')
