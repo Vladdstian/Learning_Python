1d71968954366425dba23866275c12392e554839
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-05-08 10:10:24 +0300

    updating basic info that I am learning for the second day!

--- Day 2/Basics/main.py
+++ Day 2/Basics/main.py
@@ -1,9 +1,16 @@
-# take input from the user
-
+# earn to take input from the user and to use variables
 name = input("What is your name?\n").lower().capitalize()
 input(f"Well, {name}, do you know how to take input from a user?")
-
 print(f"I will tell you my dear {name}.")
 print("You just have to use the function input() built into Python")
 print("It's that easy!")
 print(f'And, {name}, keep it a secret but you can assign it to a variable and use it whenever you want in your code')
+
+# variable naming rules
+print("""to name the variables, in python there are some rules:
+* you cannot use the built-in functions of Python as names for your variables
+* the name of a variable can have multiple names but they should be separated in Python with '_'
+* you can have numbers in the names of variables, but they shouldn't start with a number""")
+
+
+
