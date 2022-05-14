5af7f560831300e708ab914a41cc547a25e39782
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-05-14 18:48:06 +0300

    calculator project

---
+++ Day 8/Projects/Calculator/calculator.py
@@ -0,0 +1,54 @@
+# This is a calculator app
+import functions
+
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+operations = {
+    "+": functions.add_numbers,
+    "-": functions.subst_numbers,
+    "*": functions.multiply_numbers,
+    "/": functions.divide_numbers,
+    "%": functions.divide_rest
+}
+
+
+def calculator():
+    print(logo)
+    num1 = int(input("What is the first number\n"))
+    for symbol in operations:
+        print(symbol)
+    op_symbol = input("Which operation would you like to do? ")
+
+    while True:
+        num2 = int(input("What is the second number\n"))
+        result1 = operations[op_symbol](num1, num2)
+        print(f"{num1} {op_symbol} {num2} = {result1}")
+
+        continue_op = input("Would you like to do another operation?type 'y' to continue calculating, 'n' to start a "
+                            "fresh calculation or 'q' to quit\n")
+        if continue_op == 'y':
+            op_symbol = input("Which operation would you like to do? ")
+            num1 = result1
+            continue
+        elif continue_op == 'n':
+            calculator()  # this means recursion -> a function that calls itself
+        else:
+            break
+
+
+calculator()
---
+++ Day 8/Projects/Calculator/functions.py
@@ -0,0 +1,23 @@
+def add_numbers(n1, n2):
+    """Adds two numbers together"""
+    return n1 + n2
+
+
+def subst_numbers(n1, n2):
+    """Subtracts a number from another"""
+    return n1 - n2
+
+
+def multiply_numbers(n1, n2):
+    """Multiplies two numbers together"""
+    return n1 * n2
+
+
+def divide_numbers(n1, n2):
+    """Divides two numbers"""
+    return n1 / n2
+
+
+def divide_rest(n1, n2):
+    """Gives the rest of division between two numbers"""
+    return n1 % n2
