3e8076e9dc0bc5c7a4eeddd7c084831a3c8081e7
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-05-14 18:54:49 +0300

    small updates to calculator program

--- Day 8/Projects/Calculator/calculator.py
+++ Day 8/Projects/Calculator/calculator.py
@@ -29,25 +29,26 @@ operations = {
 
 def calculator():
     print(logo)
-    num1 = int(input("What is the first number\n"))
-    for symbol in operations:
-        print(symbol)
-    op_symbol = input("Which operation would you like to do? ")
-
+    num1 = float(input("What is the first number\n"))
     while True:
-        num2 = int(input("What is the second number\n"))
+        for symbol in operations:
+            print(symbol)
+        op_symbol = input("Which operation would you like to do? ")
+        num2 = float(input("Please pick another number\n"))
         result1 = operations[op_symbol](num1, num2)
         print(f"{num1} {op_symbol} {num2} = {result1}")
 
         continue_op = input("Would you like to do another operation?type 'y' to continue calculating, 'n' to start a "
                             "fresh calculation or 'q' to quit\n")
         if continue_op == 'y':
-            op_symbol = input("Which operation would you like to do? ")
             num1 = result1
             continue
         elif continue_op == 'n':
             calculator()  # this means recursion -> a function that calls itself
+        elif continue_op == 'q':
+            break
         else:
+            print("I don't understand what you want")
             break
 
 
