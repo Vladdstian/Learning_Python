dd3940acd864bbaf3213f0fea0269d094628d1e9
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-05-15 10:19:05 +0300

    update blackjack game ... work in progress

--- Day 8/Projects/Blackjack/blackjack.py
+++ Day 8/Projects/Blackjack/blackjack.py
@@ -1,5 +1,5 @@
 # https://games.washingtonpost.com/games/blackjack/
-
+import random
 logo = """
 .------.            _     _            _    _            _    
 |A_  _ |.          | |   | |          | |  (_)          | |   
@@ -22,11 +22,48 @@ def withdraw(balance):
     return 0
 
 
-# def start_game():
+def bid(balance):
+    bid_opt = {
+        1: 10,
+        2: 15,
+        3: 20,
+        4: "All-In",
+    }
+    for bids in bid_opt():
+        print(f"{bids} - ${bid_opt[bids]}")
+    choice = int(input("What is your option? 1, 2, 3, 4? \n"))
+    if choice == 4:
+        return balance
+    else:
+        return bid_opt[choice]
+
+
+def deal_cards():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return random.choice(cards)
+
+
+def game(balance):
+    value = bid(balance)
+    player = []
+    computer = []
+    while balance != 0:
+        player.append(deal_cards())
+        player.append(deal_cards())
+        computer.append(deal_cards())
+        computer.append(deal_cards())
+        print("Would you like to: 1 - Draw another card, 2 - pass")
+        if 1:
+            player.append(deal_cards())
+        else:
+
+    return balance
+
 
 def back_menu(balance):
     answer = input("Press 'b' to go back to the main menu or 'q' to quit")
     if answer == 'b':
+        print("\n\n\n")
         return True
     else:
         withdraw(balance)
@@ -36,8 +73,8 @@ def back_menu(balance):
 def menu():
     balance = 0
     game_status = True
+    print(logo)
     while game_status:
-        print(logo)
         actions = {
             1: "Add money",
             2: "Start a new game",
@@ -48,17 +85,16 @@ def menu():
             print(f"{key} - {actions[key]}")
         choice = int(input("Choose your action:\n"))
         if choice == 1:
-            add_money(balance)
+            balance = add_money(balance)
             game_status = back_menu(balance)
         elif choice == 2:
             if balance <= 0:
                 print("Please add more money in order to start playing!")
             else:
-                break
-                # start_game()
+                balance = game(balance)
         elif choice == 3:
             print(f"You have ${balance} at the table!")
-            game_status = back_menu(balance)
+            continue
         else:
             balance = withdraw(balance)
             break
