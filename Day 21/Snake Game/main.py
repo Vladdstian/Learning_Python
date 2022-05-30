5cf5e912e832545c43e0e35718f2e4f3902fd304
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-05-30 14:24:48 +0300

    split the code to make it more clear and understandable and to reflect better the oop

--- Day 21/Snake Game/main.py
+++ Day 21/Snake Game/main.py
@@ -1,22 +1,19 @@
 from turtle import Screen, Turtle
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
 
-snake = []
-position = 0
-for segment in range(3):
-    bob = Turtle(shape="square")
-    bob.color("white")
-    bob.penup()
-    bob.setposition(starting_positions[position])
-    snake.append(bob)
-    position += 1
-
-
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
 screen.exitonclick()
