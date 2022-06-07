dfa96ca56799926b77c9c5d1591840ab343e7bdf
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-06-07 17:02:12 +0300

    pandas training continuation

--- Day 27/Intermediate/main_day27.py
+++ Day 27/Intermediate/main_day27.py
@@ -1,5 +1,8 @@
 import pandas
 
+# pandas - a better way of working with CSV files
 data = pandas.read_csv("weather_data.csv")
 print(data)
 print(data["temp"])
+
+print(data["temp"])  # prints the column and automatically finds the data
