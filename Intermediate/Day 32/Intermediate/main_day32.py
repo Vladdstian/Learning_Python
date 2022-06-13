863fa0fcaa3819fffda375b76e2b5ec0bb7a5b02
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-06-13 09:28:30 +0300

    more GUI with tkinter

--- Intermediate/Day 32/Intermediate/main_day32.py
+++ Intermediate/Day 32/Intermediate/main_day32.py
@@ -9,6 +9,20 @@ window.minsize(width=500, height=500)
 # Label
 
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "italic"))
-my_label.pack(side="left")  # it is a geometry management system
+my_label.pack()  # it is a geometry management system
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+# Button
+
+
+def button_clicked():
+    my_label.config(text="Button Got clicked")
+
+
+button = tkinter.Button(text="click me!", command=button_clicked)
+button.pack()
+
 
 window.mainloop()  # it has to be at the end
