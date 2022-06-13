26c1e59a751c0d37b469552195607e3aa43a653a
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-06-13 09:22:08 +0300

    *args and **kwargs

--- Intermediate/Day 32/Intermediate/playgroud.py
+++ Intermediate/Day 32/Intermediate/playgroud.py
@@ -1,3 +1,29 @@
+# Advanced Python arguments
+
+# keyword arguments
+# def my_function(a,b,c):
+#   do this with a
+#   then do this with b
+#   finally do this with c
+
+# keyword arguments with default values
+# all the arguments are optional and when you want to call the function you are not required to provide any of them
+# def my_function(a=1,b=2,c=3):
+#   do this with a
+#   then do this with b
+#   finally do this with c
+
+# functions that can take any number of arguments
+# def add(*args):  -> this function takes as many arguments as you want.
+# the * is very important and args is just a naming convention that all python devs might use it
+# for n in args:
+#   print(n)
+# Example: playground add function
+
+# functions that can take any number of key-word arguments
+# Example: playground calculate function
+
+
 # function with unlimited arguments
 # when calling this function any number of arguments can be added
 def add(*args):  # the args is represented by a tuple
