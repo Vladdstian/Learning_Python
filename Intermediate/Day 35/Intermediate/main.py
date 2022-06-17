5127b8bbb5a1c0642d955e73e2f0f6f1ad4656b9
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-06-17 18:34:21 +0300

    learning: errors and exceptions

--- Intermediate/Day 35/Intermediate/main.py
+++ Intermediate/Day 35/Intermediate/main.py
@@ -41,9 +41,19 @@ else:
     content = file.read()
     print(content)
 finally:
-    raise
+    # raise TypeError("This is an error that I made up!")
     file.close()
     print("File was closed.")
+
+# Example 2 - raising an Error
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 meters")
+
+bmi = weight / height ** 2
+print(bmi)
 
 
 
