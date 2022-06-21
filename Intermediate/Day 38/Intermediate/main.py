902aa1843674176f8b14024f75adb8d7c4631936
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-06-21 13:47:22 +0300

    SMTP + datetime modules

--- Intermediate/Day 38/Intermediate/main.py
+++
@@ -1,8 +0,0 @@
-# sending emails with Python
-import smtplib
-
-my_email = "insert_email@gmail.com"
-
-connection = smtplib.SMTP("smtp.gmail.com")
-
-
---
+++ Intermediate/Day 38/Intermediate/smtp module/main.py
@@ -0,0 +1,18 @@
+# sending emails with Python
+import smtplib
+
+my_email = "insert_email@gmail.com"
+password = "abcd1234()"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()  # encrypting the email - secure connection
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="vlad.c.nichifor@gmail.com",
+        msg="Subject:Hello\n\nThis is the body of my email"
+        )
+
+# connection.close() - no need since we opened the connection the same way we opened a file
+# - at the end it will be closed automatically
+
